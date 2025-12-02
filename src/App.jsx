895e9c5aa38b253c519a4f5f9fcca0d4900ff8cd
Abhishek Kumar <itcc275@gmail.com>
2025-12-02 16:07:51 +0530

    updated page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import JeevanParichay from "./components/JeevanParichay";
+import { Routes, Route } from "react-router-dom";
 import NameInput from "./components/NameInput";
 
 function App() {
-  return <NameInput />;
+  return (
+    <Routes>
+      <Route path="/" element={<JeevanParichay />} />
+      <Route path="/card" element={<NameInput />} />
+    </Routes>
+  )
 }
 
 export default App;
+
